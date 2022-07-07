4195548d2cee436f5142812332a5b2304dceaaa0
dagmawibabi <1babidagi@gmail.com>
2022-07-07 17:33:13 +0300

    updating experiences

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -7,55 +7,57 @@ export const AboutMe = () => {
                     <h2 id='aboutMe'  className='sectionTitle'> <span className='linkText'> 01. </span> About Me </h2>
                     <div className='horizontalLine'></div>
                 </div>
-                <p>
-                    Hello! My name is Dagmawi and I enjoy creating tech solutions to my daily problems. My interest in software development started back in 2013 when I accidentally stumbled upon a batch programming ebook in one of my harddrives — turns out there's so much you could do on a black & white terminal!
-                </p>
-                <br></br>
-                <p>
-                    Fast-forward to today, and I’ve had the privilege of working at an 
-                    <span className='linkText'> <a href='https://ArifEvents.com' target='_blank' rel='noreferrer'> event organizing company </a> </span> , 
-                    <span className='linkText'> <a href='https://AblazeLabs.com' target='_blank' rel='noreferrer'> multiple start-ups </a> </span> ,
-                    <span className='linkText'> <a href='https://twitter.com/MoF_Ethiopia' target='_blank' rel='noreferrer'> a huge corporation </a> </span> , 
-                    and a
-                    <span className='linkText'> <a href='https://dagmawibabi.github.io/DigitalArtsClub/index.html' target='_blank' rel='noreferrer'> student-led design club </a> </span>
-                    . My main focus these days is building accessible, inclusive products and digital experiences at <span className='linkText'> <a href='https://dreamintelligence.dagmawibabi.com' target='_blank' rel='noreferrer'> Dream Intelligence </a> </span> for a variety of clients.
-                </p>
-                {/* <br></br>
-                <p>
-                    I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.
-                </p> */}
-                <br></br>
-                <p>
-                    Here are a few technologies I’ve been working with recently:
-                </p>
-                <div className='flexSpaceBetween list' style={{width: '440px', marginLeft: '15px'}}>
-                    <ul>
-                        <li> React </li>
-                        <li> Flutter </li>
-                        <li> NodeJS </li>
-                        <li> Tailwind </li>
-                        <li> MongoDB </li>
-                    </ul>
-                    <ul>
-                        <li> Sass </li>
-                        <li> Prisma </li>
-                        <li> GraphQL </li>
-                        <li> Typescript </li>
-                    </ul>
-                    <ul>
-                        <li> C# </li>
-                        <li> C++ </li>
-                        <li> Python </li>
-                    </ul>
-                    <ul>
-                        <li> Unity </li>
-                        <li> Illustrator </li>
-                    </ul>
+                <div className='someSpace'>
+                    <p>
+                        Hello! My name is Dagmawi and I enjoy creating tech solutions to my daily problems. My interest in software development started back in 2013 when I accidentally stumbled upon a batch programming ebook in one of my harddrives — turns out there's so much you could do on a black & white terminal!
+                    </p>
+                    <br></br>
+                    <p>
+                        Fast-forward to today, and I’ve had the privilege of working at an 
+                        <span className='linkText'> <a href='https://ArifEvents.com' target='_blank' rel='noreferrer'> event organizing company </a> </span> , 
+                        <span className='linkText'> <a href='https://AblazeLabs.com' target='_blank' rel='noreferrer'> multiple start-ups </a> </span> ,
+                        <span className='linkText'> <a href='https://twitter.com/MoF_Ethiopia' target='_blank' rel='noreferrer'> a huge corporation </a> </span> , 
+                        and a
+                        <span className='linkText'> <a href='https://dagmawibabi.github.io/DigitalArtsClub/index.html' target='_blank' rel='noreferrer'> student-led design club </a> </span>
+                        . My main focus these days is building accessible, inclusive products and digital experiences at <span className='linkText'> <a href='https://dreamintelligence.dagmawibabi.com' target='_blank' rel='noreferrer'> Dream Intelligence </a> </span> for a variety of clients.
+                    </p>
+                    {/* <br></br>
+                    <p>
+                        I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.
+                    </p> */}
+                    <br></br>
+                    <p>
+                        Here are a few technologies I’ve been working with recently:
+                    </p>
+                    <div className='flexSpaceBetween list' style={{width: '440px', marginLeft: '15px'}}>
+                        <ul>
+                            <li> React </li>
+                            <li> Flutter </li>
+                            <li> NodeJS </li>
+                            <li> Tailwind </li>
+                            <li> MongoDB </li>
+                        </ul>
+                        <ul>
+                            <li> Sass </li>
+                            <li> Prisma </li>
+                            <li> GraphQL </li>
+                            <li> Typescript </li>
+                        </ul>
+                        <ul>
+                            <li> C# </li>
+                            <li> C++ </li>
+                            <li> Python </li>
+                        </ul>
+                        <ul>
+                            <li> Unity </li>
+                            <li> Illustrator </li>
+                        </ul>
+                    </div>                    
                 </div>
 
             </div>
             <div className='myPhotoReveal'>
-                <div className='highlightBorder'> </div>
+                <div className='highlightBorder someSpace' > </div>
                 <img alt='myPhoto' className='myPhoto' src={require('../assets/me.png')}></img>
             </div>
         </div>
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -4,38 +4,83 @@ import { useState } from 'react';
 export const Experience = () => {
     let tabs = [
         {
-            tab: 'Tab 1',
-            tabTitle: 'Title 1',
-            duration: 'January 2020 - Present',
-            details: "1. Hello! My name is Dagmawi and I enjoy creating tech solutions to my daily problems. My interest in software development started back in 2013 when I accidentally stumbled upon a batch programming ebook in one of my harddrives — turns out there's so much you could do on a black & white terminal!",
-            list: ['List Item 1', 'List Item 2', 'List Item 3', 'List Item 4']
+            tab: 'AASTU AIR CoE',
+            role: 'Backend and DevOps Engineer',
+            tabTitle: 'Artificial Intelligence and Robotics Center of Excellence',
+            duration: 'February 2022 - Present',
+            details: "I'm a member of AASTU's Center of Excellence. It's a research and development program where I've developed multiple complex softwares and apps.",
+            list: [
+                "Write modern, performant, maintainable code for a diverse array of internal projects", 
+                "Deployed, secured and maintained multiple web apps",
+                "Wrote automating code to backup, log and maintain multiple projects",
+                "Communicate with multi-disciplinary teams of engineers, designers, testers, project managers and stakeholders", 
+            ]
         },
         {
-            tab: 'Tab 2',
-            tabTitle: 'Title 2',
-            duration: 'January - March 2020',
-            details: "2. My name is Dagmawi and I enjoy creating tech solutions to my daily problems. My interest in software development started back in 2013 when I accidentally stumbled upon a batch programming ebook in one of my harddrives — turns out there's so much you could do on a black & white terminal!",
-            list: ['List Item 5', 'List Item 6', 'List Item 7', 'List Item 8']
+            tab: 'Habesha Creative Designs',
+            role: 'CTO and Fullstack Engineer',
+            tabTitle: 'Habesha Creative Designs',
+            duration: 'October 2020 - Present',
+            details: "I'm the CTO and fullstack engineer at an Ethiopian based design studio. I've set, managed and run the technical direction for multiple product developments.",
+            list: [
+                "Align technology-related decisions with the company's goals", 
+                "Developed, implemented, managed and evaluated the company's technology resources", 
+                "Communicate technology strategy to partners and investors",
+                "Built and deployed MVPs and tech solutions for clients and internal teams"
+            ]
         },
         {
-            tab: 'Tab 3',
-            tabTitle: 'Title 3',
-            duration: 'April - June 2020',
+            tab: 'Arif Events',
+            role: 'Intern',
+            tabTitle: 'Arif Events',
+            duration: 'July - September 2019',
+            details: "I've interned at this amazing event organizing and managing tech company that taught me so much about start up business running, event planning and project management.",
+            list: [
+                "Worked closely with developers and management team to develop and document smaller features on the company's website", 
+                "Worked with the company's marketing team to create and manage event marketing campaigns", 
+                "Worked closely with the CEO to learn about product branding and business management"
+            ]
+        },
+        {
+            tab: 'Dream Intelligence',
+            role: 'Founder and CEO',
+            tabTitle: 'Dream Intelligence',
+            duration: 'November 2018 - Present',
             details: "3. I enjoy creating tech solutions to my daily problems. My interest in software development started back in 2013 when I accidentally stumbled upon a batch programming ebook in one of my harddrives — turns out there's so much you could do on a black & white terminal!",
-            list: ['List Item 9', 'List Item 8', 'List Item 7', 'List Item 6']
+            list: [
+                'Developed and shipped highly interactive web applications', 
+                'List Item 8', 
+                'List Item 7', 
+                'List Item 6'
+            ]
         },
         {
-            tab: 'Tab 4',
-            tabTitle: 'Title 4',
-            duration: 'July - December 2020',
-            details: "4. My interest in software development started back in 2013 when I accidentally stumbled upon a batch programming ebook in one of my harddrives — turns out there's so much you could do on a black & white terminal!",
-            list: ['List Item 5', 'List Item 4', 'List Item 3', 'List Item 2']
+            tab: 'Udeaz',
+            role: 'Co-Founder and CTO',
+            tabTitle: 'Udeaz',
+            duration: 'March - June 2020',
+            details: "I've co-founded and spearheaded a startup company that provides a platform for youth entrepreneurs to educate, incubate and launch their products and services to the world.",
+            list: [
+                'Developed and shipped highly interactive web applications', 
+                'List Item 8', 
+                'List Item 7', 
+                'List Item 6'
+            ]
+        },
+        {
+            tab: 'Freelance',
+            role: 'Founder and CEO',
+            tabTitle: 'Dream Intelligence',
+            duration: 'November 2018 - Present',
+            details: "3. I enjoy creating tech solutions to my daily problems. My interest in software development started back in 2013 when I accidentally stumbled upon a batch programming ebook in one of my harddrives — turns out there's so much you could do on a black & white terminal!",
+            list: ['Developed and shipped highly interactive web applications', 'List Item 8', 'List Item 7', 'List Item 6']
         },
     ]
     const [curTabContent, setCurTab] = useState(0);
+
     function tabSwitcher(e){
         console.log(e.target.id);
-        setCurTab(e.target.id);
+        setCurTab(parseInt(e.target.id));
     }
     return (
         <div id='experience'>
@@ -47,14 +92,20 @@ export const Experience = () => {
                 <div className='tabsContainer'>
                     <div className='tabs'>
                         <div className='tabList'>
-                            <p className={curTabContent === '0' ? "selectedTab" : "eachTab"} id='0' onClick={(e) => tabSwitcher(e)}> Tab 1 </p>
-                            <p className={curTabContent === '1' ? "selectedTab" : "eachTab"} id='1' onClick={(e) => tabSwitcher(e)}> Tab 2 </p>
-                            <p className={curTabContent === '2' ? "selectedTab" : "eachTab"} id='2' onClick={(e) => tabSwitcher(e)}> Tab 3 </p>
-                            <p className={curTabContent === '3' ? "selectedTab" : "eachTab"} id='3' onClick={(e) => tabSwitcher(e)}> Tab 4 </p>
+                            {
+                                tabs.map((item, index) => {
+                                    return (
+                                        <p key={index} className={curTabContent === index ? "selectedTab" : "eachTab"} id={index} onClick={(e) => tabSwitcher(e)}> {item['tab']} </p>
+                                    )
+                                })
+                            }
                         </div>
                         <div className='tabExpanded'>
+                            {/* Role */}
+                            <h2 className='tabTitle' style={{fontSize: '18px'}}> {tabs[curTabContent]['role']}  </h2>
+
                             {/* Title */}
-                            <h2 className='linkText tabTitle'> {tabs[curTabContent]['tabTitle']}  </h2>
+                            <h2 className='linkText tabTitle'> {'' + tabs[curTabContent]['tabTitle']}  </h2>
 
                             {/* Duration */}
                             <p className='tabDuration'> {tabs[curTabContent]['duration']} </p>
